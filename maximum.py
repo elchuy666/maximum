c7bad065815ea2e1cf9b92ef38717038e0cc8b8c
Jesus Emmanuel Sanchez Almaguer <correodefinitivo97@gmail.com>
2023-10-09 12:42:29 -0600

    agregado maximum

---
+++ maximum.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Maximum algorithm 
+# By: correodefinitivo97@gmail.com
+
+data = [1.0, 3.14, 6.2, 0.1, 5.3]
+
+maximum = -99999.99
+for x in data:
+    if x > maximum:
+        maximum = x
+    #print(x)
+
+print(maximum)
